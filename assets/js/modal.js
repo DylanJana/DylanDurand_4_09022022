9543801e7a8e58dc37c1ae03eec5964d0c9a4ec5
DylanJana <dylan.durand88@outlook.fr>
2022-02-11 15:27:54 +0100

    Mise en place d'une span comprenant un message d'erreur

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -10,7 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+//const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,4 +28,40 @@ let closeModal = function () {
   })
 }()
 
+let form = document.reserve
+let errorMessage = document.querySelectorAll('.error-message')
+let inputError = document.querySelectorAll('input')
+console.log(inputError)
 
+function validate() {
+  form.addEventListener("submit", function(e) {
+    function firstNameVerify () {
+      if(form.firstname.value != "" && form.firstname.value.length >= 2) {
+        return true
+      } else {
+        errorMessage[0].style.display = 'block'
+        inputError[0].style.border = '1px solid red'
+        e.preventDefault()
+        return false
+      }
+    }
+
+    function lastNameVerify () {
+      if(form.lastname.value != "" && form.lastname.value.length >= 2) {
+        console.log('felicitations')
+        return true
+      } else {
+        let errorMessage = document.querySelectorAll('.error-message')
+        errorMessage[1].style.display = 'block'
+        inputError[1].style.border = '1px solid red'
+        e.preventDefault()
+        return false
+      }
+    }
+
+    firstNameVerify()
+    lastNameVerify ()
+  })
+}
+
+validate()
