f14261c896fce12cca46c0c96324711c3d5088b8
DylanJana <dylan.durand88@outlook.fr>
2022-02-17 17:04:23 +0100

    Fin du projet 4

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -34,38 +34,39 @@ let errorMessage = document.querySelectorAll('.error-message')
 let inputError = document.querySelectorAll('input')
 
 // Cette fonction est une fonction globale qui regroupe toutes les fonctions de vérifications des champs du formulaire
-function validate() {
+let validate = function (){
   // Lorsque je soumets le formulaire, la fonction de callback se lance
   form.addEventListener("submit", function(e) {
     // Je verifie le champ #lastname, cette fonction renvoie true si la valeur de #lastname n'est pas vide
     // Et que la valeur de ce champ est supérieur ou égal à 2 caractères
-    function firstNameVerify () {
+    let firstNameVerify = function () {
       if(form.firstname.value != "" && form.firstname.value.length >= 2) {
         errorMessage[0].style.display = 'none'
         inputError[0].style.border = '0.8px solid #ccc'
-        return true
+        firstNameVerify = true
       } else {
         errorMessage[0].style.display = 'block'
         inputError[0].style.border = '1px solid red'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
-        validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        firstNameVerify = false
       }
+      return firstNameVerify
     }
     // Je verifie le champ #lastname, cette fonction renvoie true si la valeur de #lastname n'est pas vide
     // Et que la valeur de ce champ est supérieur ou égal à 2 caractères
-    function lastNameVerify () {
+    let lastNameVerify = function () {
       if(form.lastname.value != "" && form.lastname.value.length >= 2) {
         errorMessage[1].style.display = 'none'
         inputError[1].style.border = '0.8px solid #ccc'
-        return true
+        lastNameVerify = true
       } else {
         errorMessage[1].style.display = 'block'
         inputError[1].style.border = '2px solid red'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
-        validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        //validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
+        lastNameVerify = false
       }
+      return lastNameVerify
     }
     // Je verifie l'email, pour cela j'utilise un regex, je teste la valeur rentrer dans le champs et je renvoie une réponse
     function checkEmail(valueEmail) {
@@ -73,62 +74,62 @@ function validate() {
       return regex.test(valueEmail)
     }
 
-    function emailVerify () {
+    let emailVerify = function () {
       let valueEmail = document.getElementById("email").value
       // Si l'email a passé le test du regex et si elle n'est pas vide, je renvoie true
       if(checkEmail(valueEmail) && valueEmail.length !== "") {
         errorMessage[2].style.display = 'none'
         inputError[2].style.border = '0.8px solid #ccc'
-        return true
+        emailVerify = true
       // Sinon je renvoie un message d'erreur et ajouter un border rouge à mon input
       } else {
         errorMessage[2].style.display = 'block'
         inputError[2].style.border = '2px solid red'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
-        validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        //validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
+        emailVerify = false
       }
     }
 
     // Je vérifie la date d'anniversaire de la personne qui souhaite s'inscrire
-    function birthDateVerify () {
+    let birthDateVerify = function () {
       let birthDateValue = document.getElementById("birthdate").value
       // Si la date d'anniversaire est renseignée je retourne true
       if(birthDateValue) {
         errorMessage[3].style.display = 'none'
         inputError[3].style.border = '0.8px solid #ccc'
-        return true
+        birthDateVerify = true
         // Sinon j'affiche un message d'erreur et change le design de l'input
       } else {
         errorMessage[3].style.display = 'block'
         inputError[3].style.border = '2px solid red'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
         validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        birthDateVerify = false
       }
     }
 
     // Je verifie que l'utilisateur indique un nombre dans ce champs
-    function numberOfContribution () {
+    let numberOfContribution = function () {
       // Je récupére la valeur du champ ayant comme ID quantity
       let participations = document.getElementById("quantity").value
       // Si la valeur du champ ayant l'ID quantity est un nombre et il est valide alors je renvoie true
       if (!isNaN(participations) && participations) {
         errorMessage[4].style.display = 'none'
         inputError[4].style.border = '0.8px solid #ccc'
-        return true
+        numberOfContribution = true
         // Sinon je fais apparaître le message d'erreur et redesign l'input
       } else {
         errorMessage[4].style.display = 'block'
         inputError[4].style.border = '2px solid red'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
-        validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        //validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
+        numberOfContribution = false
       }
     }
 
     // Je verifie que l'utilisateur a bien choisi le tournoi auquel il veut participer
-    function tournamentChoice () {
+    let tournamentChoice = function () {
       // Si l'une des options est choisie alors renvoi true
       if(document.getElementById('location1').checked || 
       document.getElementById('location2').checked ||
@@ -137,31 +138,31 @@ function validate() {
       document.getElementById('location5').checked ||
       document.getElementById('location6').checked) {
         errorMessage[5].style.display = 'none'
-        return true
+        tournamentChoice = true
         // Sinon je fais apparaître le message d'erreur et renvoie false
       } else {
         errorMessage[5].style.display = 'block'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
-        validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        //validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
+        tournamentChoice = false
       }
     }
 
-    function checkBoxLegalVerify () {
+    let checkBoxLegalVerify = function () {
       let checkBox = document.querySelector('.checkbox2-label .checkbox-icon')
       if(document.getElementById('checkbox1').checked) {
         checkBox.style.border = "1px solid transparent"
         errorMessage[6].style.display = 'none'
-        return true
+        checkBoxLegalVerify = true
       } else {
         checkBox.style.border = "2px solid red"
         errorMessage[6].style.display = 'block'
         e.preventDefault() // J'arrête le comportement par defaut du bouton submit
-        validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
-        return false
+        //validate() // Suite à mon erreur je relance la fonction, pour permettre une nouvelle saisie
+        checkBoxLegalVerify = false
       }
     }
-
+    // J'appelle toutes mes fonctions
     firstNameVerify()
     lastNameVerify ()
     emailVerify()
@@ -169,7 +170,26 @@ function validate() {
     numberOfContribution()
     tournamentChoice()
     checkBoxLegalVerify()
+
+    // SI toutes les étapes du formulaire sont égales à TRUE alors j'affiche le message de remerciement SINON je relance la fonction validate
+    let confirmMessage = function () {
+      if (firstNameVerify && lastNameVerify && emailVerify && birthDateVerify && numberOfContribution && tournamentChoice && checkBoxLegalVerify) {
+        let modalBodyContent = document.querySelector('form')
+        let createDiv = modalBodyContent.innerHTML = '<div class ="confirm-message"><h2>Merci pour votre inscription</h2><button class="btn-submit close-modal" value="Fermer" onClick="window.location.reload()">Fermer</button></div>'
+        modalBodyContent.style.height = "889px"
+        e.preventDefault()
+        let crossReload = document.querySelector('.close')
+        crossReload.setAttribute("onClick", "window.location.reload()")
+        let buttonClose = document.querySelector('.close-modal')
+        buttonClose.addEventListener('click', function(e) {
+          modalbg.style.display = "none"
+          e.preventDefault()
+        })
+      }
+    }
+    confirmMessage()
   })
 }
-
 validate()
+document.querySelector("form").reset();
+
